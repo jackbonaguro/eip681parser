3c656ca64c7cd7e14cb81a77f014ed5b344a4426
Jack Bonaguro <jbonaguro@bitpay.com>
2019-11-04 15:46:32 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const ethUrlParser = require('./eth-url-parser');
+const ethereumjsABI = require('ethereumjs-abi');
+const EthereumTx = require('ethereumjs-tx').Transaction;
+
+const ReservedParams = [
+  'value',
+  'gas',
+  'gasLimit',
+  'gasPrice'
+];
+
+class EIP681Parser {
+  parseTest(url) {
+    let parsedUrl = ethUrlParser.parse(url);
+    let typedParams = Object.values(parsedUrl.parameters).filter((param) => {
+      return !ReservedParams.includes(param.key);
+    });
+    let types = typedParams.map(p => p.key);
+    let selector = ethereumjsABI.methodID(
+      parsedUrl.parameters.function_name,
+      types
+    );
+    let encodedValues = typedParams.map(param => {
+      return ethereumjsABI.rawEncode([param.key], [param.val]);
+    });
+    let txData = '0x' + Buffer.concat([
+      selector,
+      ...encodedValues
+    ]).toString('hex');
+    return {
+      ...parsedUrl,
+      types,
+      selector,
+      encodedValues,
+      txData
+    };
+  }
+
+  createTxTest(url, nonce) {
+    let obj = this.parseTest(url);
+    let txParams = {
+      nonce,
+      to: obj.target_address,
+      data: obj.txData
+    };
+    let parameters = Object.values(obj.parameters);
+
+    let gasPrice = parameters.find((p) => {
+      return p.key === 'gasPrice' && p.val;
+    });
+    if (gasPrice) {
+      txParams.gasPrice = gasPrice.val;
+    }
+
+    let gasLimit = parameters.find((p) => {
+      return (p.key === 'gasLimit' || p.key === 'gas') && p.val;
+    });
+    if (gasLimit) {
+      txParams.gasLimit = gasLimit.val;
+    }
+
+    let value = parameters.find((p) => {
+      return p.key === 'value' && p.val;
+    });
+    if (value) {
+      txParams.value = value.val;
+    }
+
+    if (
+      !txParams.nonce ||
+      !txParams.gasPrice ||
+      !txParams.gasLimit ||
+      !txParams.to ||
+      !txParams.value ||
+      !txParams.data
+    ) {
+      throw new Error('Missing required params for tx creation');
+    }
+
+    let unsignedTx = new EthereumTx(txParams);
+
+    return {
+      ...obj,
+      txParams,
+      unsignedTx: unsignedTx.serialize().toString('hex')
+    };
+  }
+
+  /**
+   * @name parse
+   * @description convert eip681 url to transaction data
+   * @param {String} url
+   * @returns {String} unsignedTx
+   */
+  parse(url) {
+    return this.parseTest(url).txData;
+  }
+
+  /**
+   * @name createTx
+   * @description convert eip681 url to an unsigned eth transaction
+   * @param {String} url
+   * @returns {String} unsignedTx
+   */
+  createTx(url, nonce) {
+    return this.createTxTest(url, nonce).unsignedTx;
+  }
+}
+
+module.exports.default = EIP681Parser;
---
+++ test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+
+const EIP681Parser = require('./index').default;
+
+const eip681Parser = new EIP681Parser();
+
+const testLabel = (msg) => console.log('\x1b[36m%s\x1b[0m', `\n${msg}\n`);
+testLabel('Parse txData from url');
+
+let txData = eip681Parser.parseTest(
+  'ethereum:0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7/pay?uint256=1&address=0x8e23ee67d1332ad560396262c48ffbb01f93d052&uint256=2&uint256=3'
+);
+
+console.log(txData);
+assert(txData);
+
+testLabel('Create transaction from url');
+
+let unsignedTx = eip681Parser.createTxTest(
+  'ethereum:0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7/pay?uint256=1&address=0x8e23ee67d1332ad560396262c48ffbb01f93d052&uint256=2&uint256=3&value=10000000&gasPrice=1000000000&gasLimit=90000',
+  1
+);
+
+console.log(unsignedTx);
+assert(unsignedTx);
